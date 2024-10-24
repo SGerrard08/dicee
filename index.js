ae2b8dbb622f75d0000b1b9766f94176e0703eab
SGerrard08 <gojiin@hotmail.com>
2024-10-24 20:57:30 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ let randomNumber2 = Math.random() * 6;
     document.querySelector(".img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png");
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩 Player 1 Win!";
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
 } 
 
 else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 2 Win! 🚩";
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 } 
 
 else {
